f6255db4150b9d32c4fcdae7fca29f096773c745
Mariano Alvarez <malvarezhcr@gmail.com>
2020-02-15 22:33:14 -0600

    chore(ci): test workflow

--- projects/store/src/lib/store.service.spec.ts
+++ projects/store/src/lib/store.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Store } from './store.service';
+import { NanuxStore } from './nanux-store.module';
 
 describe('StoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      NanuxStore.forRoot({
+        debugMode: true,
+        decorators: true
+      })
+    ]
+  }));
 
   it('should be created', () => {
     const service: Store = TestBed.get(Store);
